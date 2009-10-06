54c5029e6b2c39482d2a038360e0bc681a5e4af3
Ivar Abrahamsen <ivar@flurdy.com>
2009-10-06 18:14:53 +0200

    layout laid out

---
+++ src/main/java/com/flurdy/grid/fotogator/GridPage.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.flurdy.grid.fotogator;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ *
+ * @author ivar
+ */
+public class GridPage extends WebPage {
+    public GridPage() {
+        super ();
+    }
+
+    public GridPage(PageParameters params) {
+        //TODO:  process page parameters
+    }
+}
+
