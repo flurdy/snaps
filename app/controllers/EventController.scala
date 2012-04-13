b03b63d02ad1efd4eb960d2fe1146c1c9be22185
Ivar Abrahamsen <ivar.abrahamsen@tieto.com>
2012-04-13 19:55:52 +0100

    create static flow

--- app/controllers/EventController.scala
+++ app/controllers/EventController.scala
@@ -9,14 +9,21 @@ import play.Logger
 object EventController extends Controller {
 
   val searchForm: Form[String] = Form(
-    "searchText" -> nonEmptyText(maxLength = 100)
+    "searchText" -> text(maxLength = 100)
+  )
+  val createForm: Form[String] = Form(
+    "eventName" -> nonEmptyText(maxLength = 100)
+  )
+
+  val updateForm: Form[String] = Form(
+    "eventName" -> nonEmptyText(maxLength = 100)
   )
 
   def search = Action {  implicit request =>
     searchForm.bindFromRequest.fold(
       errors => {
         Logger.warn("Bad request:"+errors)
-        BadRequest(views.html.index(errors))
+        BadRequest(views.html.index(errors,createForm))
       },
       searchText => {
         Logger.info("Find event")
@@ -29,5 +36,37 @@ object EventController extends Controller {
     Ok(views.html.events.view())
   }
 
+
+  def createEvent = Action { implicit request =>
+    createForm.bindFromRequest.fold(
+      errors => {
+        Logger.warn("Bad request:"+errors)
+        BadRequest(views.html.index(errors,createForm))
+      },
+      eventName => {
+        Logger.info("Create event")
+        Redirect(routes.EventController.showEditEvent(1));
+      }
+    )
+  }
+
+  def showEditEvent(eventId: Long) = Action {
+    Ok(views.html.events.edit())
+  }
+
+
+
+  def updateEvent(eventId: Long) = Action { implicit request =>
+    updateForm.bindFromRequest.fold(
+      errors => {
+        Logger.warn("Bad request:"+errors)
+        BadRequest(views.html.index(errors,createForm))
+      },
+      eventName => {
+        Logger.info("Update event")
+        Redirect(routes.EventController.viewEvent(1));
+      }
+    )
+  }
 
 }
