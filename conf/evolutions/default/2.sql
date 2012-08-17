a8fa58f6e3e3791fc1f7d53318fb1976824183c8
Ivar Abrahamsen <ivar@flurdy.com>
2012-08-18 00:28:09 +0100

    scroll event list

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -4,10 +4,9 @@
 
 DELETE FROM participant;
 
-INSERT INTO participant (participantid,username,fullname,email,password) VALUES (
+INSERT INTO participant (participantid,username,fullname,email,password,admin) VALUES (
       (SELECT NEXTVAL('participant_seq')),
-       'testuser', 'Test User', 'test@example.com', '$2a$10$waIvJd.49bI.OhwgmKIO2uBhAP4KMoCGGdx/at2kjIE6IFdjmWV6e' );
-
+       'testuser', 'Test User', 'test@example.com', '$2a$10$waIvJd.49bI.OhwgmKIO2uBhAP4KMoCGGdx/at2kjIE6IFdjmWV6e',true );
 
 
 
--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -9,7 +9,7 @@ DELETE FROM snapalbum;
 INSERT INTO snapalbum (albumid,publisher,url,eventid) VALUES (
         (SELECT NEXTVAL('snapalbum_seq')),
          'John Smith', 'http://flickr.com/photos/flurdy/set/12121eqweewqwqe',
-        (SELECT MAX(eventid) FROM snapevent WHERE eventname = 'Christmas') );
+        (SELECT MAX(eventid) FROM snapevent ) );
 
 
 # --- !Downs
